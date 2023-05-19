6c664dc313041491b898edf7018c21e168fb4ee9
Christian Magnus Bjoerk Ingul <106429512+christianingul@users.noreply.github.com>
2023-05-18 19:11:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 
 import streamlit as st
-import subprocess
-
-# Install langchain
-subprocess.check_call(["pip", "install", "langchain"])
-
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 
